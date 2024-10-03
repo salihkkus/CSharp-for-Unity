2689b5988b81cefa537009bd8e16a9318a10df84
salihkarakus <sskarakussalih77@gmail.com>
2024-10-03 13:45:50 +0300

    gettinginput created

---
+++ Basics/2-GettingInput/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace gettinginput
+{
+    class gettinginput
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How old are you?");
+            string input = Console.ReadLine();
+            Console.WriteLine("You are " + input + " years old");
+        }
+    }
+}
