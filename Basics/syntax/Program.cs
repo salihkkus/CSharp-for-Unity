ff4142f1c58c6deaa8e6331f3fe128868b9f0382
salihkarakus <sskarakussalih77@gmail.com>
2024-10-03 11:37:27 +0300

    syntax created

---
+++ Basics/syntax/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace helloworld
+{
+    class helloworld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Its my first script");
+        }
+    }
+}
