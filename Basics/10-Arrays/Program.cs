86b7e111a1b4c3d7ad0f138d0708e3f0e36fd0c5
salihkarakus <sskarakussalih77@gmail.com>
2024-10-05 20:48:23 +0300

    Arrays created

--- Basics/10-Arrays/Program.cs
+++ Basics/10-Arrays/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace BuildingArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] summerStrut;
+
+            summerStrut = new string[] { "Juice", "Missing U", "Touch the Sky", "New York Groove", "Make Me Feel", "Bound 2", "Despacito", "on Sight" };
+
+            int[] ratings = { 5, 4, 4, 3, 3, 5, 5, 4 };
+
+
+            if (summerStrut.Length == 8)
+            {
+                Console.WriteLine("SummerStrut playlist is ready to go!");
+            }
+            else if (summerStrut.Length > 8)
+            {
+                Console.WriteLine("Too many songs!");
+            }
+            else
+            {
+                Console.WriteLine("Add some songs!");
+            }
+
+            Console.WriteLine($"You rated the song {summerStrut[1]} {ratings[1]} stars");
+
+            summerStrut[7] = "I Like It";
+
+            ratings[7] = 3;
+
+            int threeRating = Array.IndexOf(ratings, 3);
+            Console.WriteLine($"Song number {threeRating + 1} is rated three stars.");
+
+            Array.Reverse(summerStrut);
+            string newFirst = summerStrut[0];
+            string newLast = summerStrut[7];
+            Console.WriteLine($"The first song in the playlist is now {newFirst}.");
+            Console.WriteLine($"The last song in the playlist is now {newLast}.");
+
+            Array.Sort(summerStrut);
+            string firstSong = summerStrut[0];
+            string lastSong = summerStrut[7];
+            Console.WriteLine($"The first song in the playlist is now {firstSong}.");
+            Console.WriteLine($"The last song in the playlist is now {lastSong}.");
+        }
+    }
+}
