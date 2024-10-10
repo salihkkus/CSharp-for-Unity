619572f2f1c310b83f948a3a30cf00a4cc0cc838
salihkarakus <sskarakussalih77@gmail.com>
2024-10-10 14:54:59 +0300

    classes created

---
+++ OOP/Classes/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OverloadingConstructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Forest f = new Forest("Congo", "Tropical");
+            f.trees = 0;
+
+            Forest rendlesham = new Forest("Rendlesham");
+            Console.WriteLine(rendlesham.biome);
+        }
+    }
+}
---
+++ OOP/Classes/forest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace OverloadingConstructors
+{
+    class Forest
+    {
+        public string name;
+        public int trees;
+        public int age;
+        public string biome;
+
+        public Forest(string name, string biome)
+        {
+            this.name = name;
+            this.biome = biome;
+            age = 1;
+        }
+
+        public Forest(string name) : this(name, "Unknown")
+        {
+            Console.WriteLine("No biome provided, using default value \"Unknown\".");
+        }
+
+        public int Grow()
+        {
+            this.trees += 30;
+            this.age++;
+            return this.trees;
+        }
+
+        public int Burn()
+        {
+            trees -= 20;
+            age++;
+            return trees;
+        }
+    }
+
+}
