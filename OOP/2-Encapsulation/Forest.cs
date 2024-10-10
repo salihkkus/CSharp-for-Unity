72d7e71a06291acb6134f96ff8d59145f3cc5153
salihkarakus <sskarakussalih77@gmail.com>
2024-10-11 00:56:15 +0300

    encapsulation edited

---
+++ OOP/2-Encapsulation/Forest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace GetOnlyProperties
+{
+    class Forest
+    {
+        public int Age
+        {
+            get; private set;
+        }
+
+        public string Name
+        { get; set; }
+
+        public int Trees
+        { get; set; }
+
+        private string biome;
+        public string Biome
+        {
+            get
+            {
+                return biome;
+            }
+            set
+            {
+                string[] validBiomes = { "Tropical", "Temperate", "Boreal" };
+                if (Array.IndexOf(validBiomes, value) >= 0)
+                {
+                    biome = value;
+                }
+                else
+                {
+                    biome = "Unknown";
+                }
+            }
+        }
+    }
+
+}
---
+++ OOP/2-Encapsulation/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GetOnlyProperties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Forest f = new Forest();
+            f.Name = "Congo";
+            f.Trees = 0;
+            f.Biome = "Tropical";
+            f.Age = 10;
+        }
+    }
+}
