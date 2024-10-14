3c4dedc31cd6c41b6e6b91d211dbda7903dd438f
salihkarakus <sskarakussalih77@gmail.com>
2024-10-14 17:10:19 +0300

    interfaces created

---
+++ OOP/5-Interfaces/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LearnInterfaces
+{
+    interface IAutomobile
+    {
+        string LicensePlate { get; }
+        double Speed { get; }
+        int Wheels { get; }
+        void Honk();
+    }
+}
