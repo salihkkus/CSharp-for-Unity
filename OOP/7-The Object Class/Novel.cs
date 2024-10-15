c200411823f26d0b1692817be7953b28942f722f
salihkarakus <sskarakussalih77@gmail.com>
2024-10-16 00:45:53 +0300

    object class created

---
+++ OOP/7-The Object Class/Novel.cs
@@ -0,0 +1,33 @@
+﻿namespace TheObjectClass
+{
+    class Novel : Book
+    {
+        public int CurrentPage
+        { get; set; }
+
+        public Novel(int page = 0) : base()
+        {
+            CurrentPage = page;
+        }
+
+        public Novel(int page, string author, string title) : base(author, title)
+        {
+            CurrentPage = page;
+        }
+
+        public void Flip()
+        {
+            CurrentPage++;
+        }
+
+        public override string Stringify()
+        {
+            return "This is a Novel object!";
+        }
+
+        public override string ToString()
+        {
+            return "Surprise!";
+        }
+    }
+}
---
+++ OOP/7-The Object Class/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TheObjectClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book bk = new Book("Ta-Nehisi Coates", "Between the World and Me");
+            Novel nl = new Novel(38);
+            Random rand = new Random();
+            int i = 9;
+
+            Object[] objects = { bk, nl, rand, i };
+
+            foreach (Object obj in objects)
+            {
+                Console.WriteLine(obj.GetType());
+                Console.WriteLine(obj);
+            }
+
+            bool b = true;
+            Console.WriteLine(b);
+            Console.WriteLine(b.ToString());
+        }
+    }
+}
